ad5971f115cbe34c9f983beb4773a6c1a1f724e3
Mike Graves <mgraves@mit.edu>
2019-12-07 17:15:43 -0500

    Add day 2

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/urfave/cli/v2"
+	"io"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -33,6 +35,24 @@ func main() {
 					}
 				}
 				fmt.Println(total)
+			case "2.1":
+				tape := make_tape(os.Stdin)
+				output := compute(12, 2, tape)
+				fmt.Println(output)
+			case "2.2":
+				tape := make_tape(os.Stdin)
+				t := make([]int, len(tape))
+			Loop:
+				for n := 0; n < 100; n++ {
+					for v := 0; v < 100; v++ {
+						copy(t, tape)
+						output := compute(n, v, t)
+						if output == 19690720 {
+							fmt.Println(100*n + v)
+							break Loop
+						}
+					}
+				}
 			default:
 				cli.ShowAppHelp(c)
 			}
@@ -43,4 +63,38 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func make_tape(r io.Reader) []int {
+	reader := bufio.NewReader(r)
+	input, _ := reader.ReadString('\n')
+	codes := strings.Split(input[:len(input)-1], ",")
+	tape := make([]int, len(codes))
+	for idx, i := range codes {
+		tape[idx], _ = strconv.Atoi(i)
+	}
+	return tape
+}
+
+func compute(n int, v int, tape []int) int {
+	var pos int
+	tape[1] = n
+	tape[2] = v
+Loop:
+	for {
+		code := tape[pos]
+		out := tape[pos+3]
+		x := tape[tape[pos+1]]
+		y := tape[tape[pos+2]]
+		switch code {
+		case 1:
+			tape[out] = x + y
+		case 2:
+			tape[out] = x * y
+		case 99:
+			break Loop
+		}
+		pos += 4
+	}
+	return tape[0]
 }
